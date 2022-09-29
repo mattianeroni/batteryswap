6578673d993eadd1a837bc461d2f24bfdec4c6a0
mattia.neroni <mattianeroni@yahoo.it>
2022-09-29 11:36:39 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='batteriesswap',
+    version='0.0.1',
+    description='A discrete event simulation showing that the future for electric vehicles is batteries sharing.',
+    author='Mattia Neroni, Ph.D, Eng.',
+    author_email='mattianeroni@yahoo.it',
+    url='https://github.com/mattianeroni/server-side-algorithms',
+    package_dir = {
+        'simulation': 'simulation'
+    },
+    packages=[
+        'simulation'
+    ],
+    python_requires='>=3.9',
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+    ]
+)
