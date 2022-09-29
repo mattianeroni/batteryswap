53871393971d5ea94bbd1c46252bc3692da5467a
mattia.neroni <mattianeroni@yahoo.it>
2022-09-29 18:26:44 +0200

    readme

---
+++ simulation/graph.py
@@ -0,0 +1,15 @@
+"""
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+This file is part of the Batteries Swap, a simulation of roads networks traffic to 
+prove that the future of electric vehicles lies on batteries sharing.
+
+Author: Mattia Neroni, Ph.D, Eng. (Sep 2022)
+Project' link: https://github.com/mattianeroni/batteriesswap
+Author's website: https://mattianeroni.github.io
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
+
+
+class Graph:
+    pass
+    
